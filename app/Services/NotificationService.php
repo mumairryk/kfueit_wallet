a982a32b1c930aaa21228dd54a26efdb5418b0e9
mumairryk <mumairryk@hotmail.com>
2023-02-20 10:29:28 +0500

    20/2/2023

---
+++ app/Services/NotificationService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+use Illuminate\Support\Facades\Http;
+class NotificationService
+{
+    public function sendOTPViaEmail($email, $otp)
+    {
+
+    }
+    public function sendOTPViaSMS($phoneNo,$opt)
+    {
+
+    }
+}
